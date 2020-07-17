7dd6b78d841a4243f67cdb8ac1786e6d71d73f73
dzu <dzu@ZEPHYRKA>
2020-07-17 17:40:43 +0300

    Started tricks system: Added basic ITrick interface to implement all tricks. Added IExecutableTrick interaface to indicate than trick has some code to execute once. Added IServerTrick interface to indicate that trick does something on server. Added AbstractTrick class to extends all my tricks from it.

---
+++ Wingx-1.16/src/main/java/dzuchun/wingx/trick/IServerTrick.java
@@ -0,0 +1,9 @@
+package dzuchun.wingx.trick;
+
+/**
+ * Instances of this interface will be send to server for update.
+ * @author dzu
+ */
+public interface IServerTrick extends ITrick {
+	
+}
---
+++ Wingx-1.16/src/main/java/dzuchun/wingx/trick/ITrick.java
@@ -0,0 +1,21 @@
+package dzuchun.wingx.trick;
+
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+public interface ITrick extends IForgeRegistryEntry<AbstractTrick> {
+	/**
+	 * Reads trick data from buffer.
+	 * @param <A> Trick class.
+	 * @param buf Buffer to read from.
+	 * @return Trick that was readed (You may just "return this").
+	 */
+	ITrick readFromBuf(PacketBuffer buf);
+	/**
+	 * Writes trick data to buffer.
+	 * @param <A> Trick class.
+	 * @param buf Buffer write to.
+	 * @return Trick that was writen (You may just "return this").
+	 */
+	ITrick writeToBuf(PacketBuffer buf);
+}
