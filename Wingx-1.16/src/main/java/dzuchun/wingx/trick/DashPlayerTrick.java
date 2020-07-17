c258eb3c9e0156a6124198e336394aac7f692525
dzu <dzu@ZEPHYRKA>
2020-07-17 17:48:14 +0300

    Added DashPlayerTrick - trick that allows player to dash in basic direction.

---
+++ Wingx-1.16/src/main/java/dzuchun/wingx/trick/DashPlayerTrick.java
@@ -0,0 +1,113 @@
+package dzuchun.wingx.trick;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import dzuchun.wingx.Wingx;
+import dzuchun.wingx.net.TrickPerformedMessage;
+import dzuchun.wingx.net.WingxPacketHandler;
+import dzuchun.wingx.util.Facing;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.network.PacketDistributor;
+
+public class DashPlayerTrick extends PlayerTrick implements IExecutableTrick, IServerTrick {
+	private static final ResourceLocation REGISTRY_NAME = new ResourceLocation(Wingx.MOD_ID, "dash_player_trick");
+	private static final Logger LOG = LogManager.getLogger();
+
+	private Facing facing = null;
+	private double strength = 0.0d;
+	private boolean nullifiesSpeed = false;
+	private boolean succesfull = true;
+
+	public DashPlayerTrick() {
+		super();
+		setRegistryName(REGISTRY_NAME);
+	}
+	
+	/**
+	 * @param caster Player that casts dash.
+	 * @param facing Facing of cast.
+	 * @param strenth Speed modification absolute value.
+	 * @param nullifiesSpeed Defines if speed should be nullified before dash.
+	 */
+	public DashPlayerTrick(PlayerEntity caster, Facing facing, double strength, boolean nullifiesSpeed) {
+		super(caster);
+		setRegistryName(REGISTRY_NAME);
+		if (facing != null) {
+			this.facing = facing;
+		} else {
+			this.facing = Facing.FORWARD;
+		}
+		this.strength = strength;
+		this.nullifiesSpeed = nullifiesSpeed;
+	}
+
+	@Override
+	public void execute(LogicalSide side, World worldIn) {
+		if (succesfull || side == LogicalSide.SERVER) {
+			if (hasCaster(worldIn)) {
+				PlayerEntity caster = getCaster(worldIn);
+				caster.fallDistance = 0.0f;
+				Vector3d motionChange = caster.getForward().scale(strength);
+				motionChange = facing.transform(motionChange);
+				if (!nullifiesSpeed) {
+					motionChange = motionChange.add(caster.getMotion());
+				}
+				caster.velocityChanged = true;
+				caster.setMotion(motionChange.x, motionChange.y, motionChange.z);
+				if (side == LogicalSide.CLIENT && Minecraft.getInstance().player.equals(caster)) {
+					Minecraft minecraft = Minecraft.getInstance();
+					if (succesfull) {
+						minecraft.player.sendStatusMessage(new TranslationTextComponent("dash.success")
+							.func_230530_a_(Style.field_240709_b_.func_240712_a_(TextFormatting.AQUA)), true);
+					} else {
+						minecraft.player.sendStatusMessage(new TranslationTextComponent("dash.fail")
+								.func_230530_a_(Style.field_240709_b_.func_240712_a_(TextFormatting.DARK_RED)), true);
+						//TODO specify reason
+					}
+				} else if (side == LogicalSide.SERVER) {
+					this.succesfull = true;
+					WingxPacketHandler.INSTANCE.send(
+							PacketDistributor.TRACKING_CHUNK.with(() -> worldIn.getChunk(caster.chunkCoordX, caster.chunkCoordZ)),
+							new TrickPerformedMessage(this));
+				}
+			} else {
+				LOG.warn("No caster found");
+			}
+		}
+	}
+
+	
+	@Override
+	public ITrick readFromBuf(PacketBuffer buf) {
+		facing = Facing.getByInt(buf.readInt());
+		strength = buf.readDouble();
+		nullifiesSpeed = buf.readBoolean();
+		succesfull = buf.readBoolean();
+		return super.readFromBuf(buf);
+	}
+	
+	@Override
+	public ITrick writeToBuf(PacketBuffer buf) {
+		buf.writeInt(facing.toInt());
+		buf.writeDouble(strength);
+		buf.writeBoolean(nullifiesSpeed);
+		buf.writeBoolean(succesfull);
+		return super.writeToBuf(buf);
+	}
+
+	@Override
+	public boolean executedSuccesfully() {
+		return succesfull;
+	}
+
+}
