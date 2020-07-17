e7feda09516a04ff195864f2599a7afcfc10bb74
dzu <dzu@ZEPHYRKA>
2020-07-17 17:46:13 +0300

    Added tricks registry.

--- Wingx-1.16/src/main/java/dzuchun/wingx/Wingx.java
+++ Wingx-1.16/src/main/java/dzuchun/wingx/Wingx.java
@@ -9,6 +9,7 @@ import dzuchun.wingx.client.input.KeyEvents;
 import dzuchun.wingx.client.render.entity.WingsRenderer;
 import dzuchun.wingx.init.EntityTypes;
 import dzuchun.wingx.net.WingxPacketHandler;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -22,6 +23,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod.EventBusSubscriber(bus = Bus.MOD, modid = Wingx.MOD_ID)
 public class Wingx {
 	public static final String MOD_ID = "wingx";
+	public static final ResourceLocation TRICKS_REGISTRY_NAME = new ResourceLocation(MOD_ID, "trick");
 
 	private static final Logger LOG = LogManager.getLogger();
 
@@ -30,7 +32,7 @@ public class Wingx {
 
 		LOG.debug("Initing");
 		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		EntityTypes.init(modEventBus);
+		EntityTypes.registerEntityTypes(modEventBus);
 		LOG.debug("Inited");
 	}
 
@@ -58,7 +60,7 @@ public class Wingx {
 		LOG.info("Performing client setup");
 
 		LOG.debug("Binding renderers");
-		RenderingRegistry.registerEntityRenderingHandler(EntityTypes.WINGS_ENTITY_TYPE.get(), WingsRenderer::new);
+		RenderingRegistry.registerEntityRenderingHandler(EntityTypes.wings_entity_type.get(), WingsRenderer::new);
 		LOG.debug("Binded renderers");
 
 		LOG.debug("Registering key bindings");
